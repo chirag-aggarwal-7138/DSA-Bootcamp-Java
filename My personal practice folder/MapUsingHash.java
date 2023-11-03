89d0bc7d487e999bae10568e2ec775323e717344
Chirag Aggarwal <aggarwalchirag728@gmail.com>
2023-11-03 06:48:05 +0000

    changed hashmap

--- My personal practice folder/MapUsingHash.java
+++ My personal practice folder/MapUsingHash.java
@@ -6,6 +6,31 @@ public class MapUsingHash {
         entities = new Entity[100];
     }
 
+    public void put(String key, String value) {
+        int hash = Math.abs(key.hashCode() % entities.length);
+        entities[hash] = new Entity(key, value);
+    }
+
+    public String get(String key) {
+        int hash = Math.abs(key.hashCode() % entities.length);
+
+        if (entities[hash] != null && entities[hash].key.equals(key)) {
+            return entities[hash].value;
+        }
+
+        return null;
+    }
+
+    public void remove(String key) {
+        int hash = Math.abs(key.hashCode() % entities.length);
+
+        if (entities[hash] != null && entities[hash].key.equals(key)) {
+            entities[hash] = null;
+        }
+
+        return;
+    }
+
     private class Entity {
         String key;
         String value;
