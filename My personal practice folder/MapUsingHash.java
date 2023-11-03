c54398af2e0b898681f2c245401f7d85d4042e65
Chirag Aggarwal <aggarwalchirag728@gmail.com>
2023-11-03 06:37:42 +0000

    heaps and hashmaps

---
+++ My personal practice folder/MapUsingHash.java
@@ -0,0 +1,18 @@
+public class MapUsingHash {
+    
+    private Entity[] entities;
+
+    public MapUsingHash() {
+        entities = new Entity[100];
+    }
+
+    private class Entity {
+        String key;
+        String value;
+
+        public Entity(String key, String value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+}
---
+++ My personal practice folder/Rough.java
@@ -0,0 +1,12 @@
+import java.util.HashMap;
+
+public class Rough {
+    public static void main(String[] args) {
+        HashMap<String, Integer> map = new HashMap<>();
+
+        map.put("Chirag", 27);
+        map.put("Aarshi", 18);
+
+        System.out.println(map);
+    }
+}
