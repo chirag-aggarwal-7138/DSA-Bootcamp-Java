42635408912b28afd88af991362c7f83dd9868a2
Chirag Aggarwal <aggarwalchirag728@gmail.com>
2023-11-24 06:49:24 +0000

    modified some files

--- My personal practice folder/Rough.java
+++ My personal practice folder/Rough.java
@@ -1,8 +1,11 @@
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.Map;
 
 public class Rough {
     public static void main(String[] args) {
         HashMap<String, Integer> map = new HashMap<>();
+        Map
 
         map.put("Chirag", 27);
         map.put("Aarshi", 18);
@@ -12,5 +15,26 @@ public class Rough {
         String name = "civo";
         int code = name.hashCode();
         System.out.println(code);
+
+        int[] nums = {1, 1, 2, 2, 2, 3, 3};
+
+        Arrays.sort(nums);
+
+        int i = 0;
+        int j = 1;
+        int count = 0;
+
+        while (j < nums.length) {
+            if (nums[i] == nums[j]) {
+                j++;
+            } else {
+                count++;
+                i++;
+                nums[i] = nums[j];
+            }
+        }
+
+        System.out.println(count + 1);
+        System.out.println(Arrays.toString(nums));
     }
 }
