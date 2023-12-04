cd68d59d3603e12c08e7d114220462971299ba21
Chirag Aggarwal <aggarwalchirag728@gmail.com>
2023-12-04 08:08:34 +0000

    added some files

---
+++ My personal practice folder/Heaps.java
@@ -0,0 +1,19 @@
+import java.util.Collections;
+import java.util.PriorityQueue;
+import javafx.util.Pair;
+
+public class Heaps {
+    public static void main(String[] args) {
+        PriorityQueue<Integer> heap = new PriorityQueue<>();
+        heap.add(5);
+        heap.add(7);
+        heap.add(9);
+        heap.add(1);
+        heap.add(2);
+        System.out.println(heap);
+        heap.poll();
+        System.out.println(heap);
+
+        Pair
+    }
+}
---
+++ My personal practice folder/OOP.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+
+public class OOP {
+
+    class hello {
+
+    }
+
+    Male chirag = new Male(null);
+    class Male {
+        String name;
+        public Male(String name) {
+            this.name = name;
+        }
+    }
+    public static void main(String[] args) {
+        OOP chirag = new OOP();
+        
+    }
+}
+
+class hello {
+
+}
